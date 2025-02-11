eb01c89f6c8f8a6f11e1a08a5d633fe8bc336201
Gleyson Willian Bruno Costa Rocha <gleysonwillian14@gmail.com>
2025-02-11 17:41:27 -0300

    commit numeros primos

--- Exercicio01/numeros_primos.cs
+++ Exercicio01/numeros_primos.cs
@@ -1,6 +1,31 @@
 ﻿using System;
-class numeros_primos{
-    static void Main(){
-            
+
+class numeros_primos
+{
+    static bool EhPrimo(int num)
+    {
+        if (num < 2) return false;
+        for (int i = 2; i * i <= num; i++)
+        {
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
+
+    static void Main()
+    {
+        Console.Write("Digite a quantidade de números primos desejada: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int contador = 0, numero = 2;
+        while (contador < n)
+        {
+            if (EhPrimo(numero))
+            {
+                Console.Write(numero + " ");
+                contador++;
+            }
+            numero++;
+        }
     }
 }
